4557266b703c93a684db99cbcf51f929ea8eaf60
safrizam1116 <safrizam1116@gmail.com>
2025-07-10 23:58:07 +0700

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -1,5 +1,1 @@
-import gdown
-
-def download_from_gdrive(file_id, output_path):
-    url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url, output_path, quiet=False)
+# downloader.py (placeholder)
